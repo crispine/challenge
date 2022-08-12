01d216c8a425661b491e055fdb5de1d4744fd3ee
crispine <gutierrezales@hotmail.com>
2022-08-12 21:38:49 +0200

    start home page

---
+++ screens/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import styles from './style';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hey</Text>
+    </View>
+  );
+}
